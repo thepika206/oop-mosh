03bdc7b8311d58d2b7a603a6241104c37b71400f
Patrick Cheung <patrick@inquired.org>
2025-05-19 11:53:55 -0500

    factory functions vs constructor functions

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+// There are two ways to create the circle object
 
 
+// Factory Function
 function createCircle(radius) {
     return {
         radius,
-        draw: function() {
-            console.log('draw', radius)
+        draw: () => {
+            console.log('draw')
         }
     }
 }
 
 const circle = createCircle(1)
 
-circle.draw();
+// Constructor function
+
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = () => {
+        console.log('draw')
+    }
+}
+
+// important, without the new key word, reference to this is the window object or global object.
+const another = new Circle(1);
