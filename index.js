cbf3480b87cf1ab291c7e72bb5b91bea6f60961f
Patrick Cheung <patrick@inquired.org>
2025-05-19 15:15:36 -0500

    getter and setter

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 function Circle(radius) {
     // private props and methods
     let defaultLocation = { x: 0, y: 0};
-    
-
 
     let computeOptimumLocation = function(factor) {
         
@@ -12,8 +10,22 @@ function Circle(radius) {
     // public props and methods
     this.radius = radius;
 
+    this.getDefaultLocation = function() {
+        return defaultLocation
+    }
 
-
+    // define getter and setter
+    Object.defineProperty(this, 'defaultLocation', {
+        get: function() {
+            return defaultLocation
+        },
+        set: function(value) {
+            if (!value.x || !value.y) {
+                throw new Error('Invalid Location.');
+            }
+            defaultLocation = value;
+        } 
+    })
 
     this.draw = function() {
         computeOptimumLocation(0.1)
@@ -23,5 +35,6 @@ function Circle(radius) {
 }
 
 const circle = new Circle(10)
-
-// circle.computeOptimumLocation -> undefined
+circle.defaultLocation // {x:1, y:1}
+circle.defaultLocation = {x:2, y:2} // {x:2, y:2}
+circle.defaultLocation = 1;  // error invalid location
